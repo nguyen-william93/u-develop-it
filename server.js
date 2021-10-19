a3f9fd6c4b93711e57ccfe8d9acda0b260e835fd
nguyen-william93 <=>
2021-10-19 14:13:06 -0500

    establish connection to the database

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'vnn195245',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
 
 app.use((req, res) => {
     res.status(404).end();
